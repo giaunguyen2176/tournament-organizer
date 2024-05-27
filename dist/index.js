53c16af77cb7f5e291a52f3b7d03055784bf5f8e
Giau Nguyen <minhgiau.nguyen@gmail.com>
2024-05-27 15:40:35 +0700

    with dist

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,5 @@
 import { Manager } from './components/Manager.js';
-
 export default Manager;
-
 export * from './components/index.js';
 export * from './interfaces/index.js';
+//# sourceMappingURL=index.js.map
